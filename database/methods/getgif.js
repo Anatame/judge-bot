7c8a22a96ae15a6683a3f1a0bcec4b2428b936d6
Anatame <anata.me97@gmail.com>
2021-02-05 18:56:58 +0530

    Added top feature

--- database/methods/getgif.js
+++ database/methods/getgif.js
@@ -6,8 +6,8 @@ exports.get = async function(term){
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${gifAPI}&q=${term}&limit=25&offset=0&rating=g&lang=en`)
     const res = await response.json() || 'error';
 
-    var totalRes = res.data.length;
-    var responseIndex = Math.floor((Math.random() * 10) + 1 ) % totalRes;
+    let totalRes = res.data.length;
+    let responseIndex = Math.floor((Math.random() * 10) + 1 ) % totalRes;
 
     const gifURL = res.data[responseIndex].images.original.url;
 
